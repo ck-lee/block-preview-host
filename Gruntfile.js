cf0df00339bd74f6a8b5ddc5b32a66987274636c
CK <ck.lee@automattic.com>
2019-06-27 13:16:56 +0800

    Implemented Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,122 @@
+/* global module:false, require:function, process:object */
+
+var webpack       = require( 'webpack' ),
+	webpackConfig = require( './webpack.config' );
+
 module.exports = function( grunt ) {
 
-	'use strict';
-
-	// Project configuration
-	grunt.initConfig( {
-
-		pkg: grunt.file.readJSON( 'package.json' ),
-
-		addtextdomain: {
+	grunt.initConfig({
+		webpack: {
+			options: webpackConfig,
+			build: {
+				plugins: webpackConfig.plugins.concat(
+					new webpack.optimize.DedupePlugin(),
+					new webpack.DefinePlugin( {
+						'process.env.NODE_ENV': JSON.stringify( 'production' )
+					} ),
+					new webpack.optimize.UglifyJsPlugin( {
+						compress: { warnings: false }
+					} )
+				),
+				output: {
+					path: 'js/'
+				}
+			},
+			'build-dev': {
+				devtool: 'sourcemap',
+				debug: true
+			},
+			watch: {
+				devtool: 'sourcemap',
+				debug: true,
+				watch: true,
+				keepalive: true
+			}
+		},
+		sass_globbing: {
+			itcss: {
+				files: (function() {
+					var files = {};
+					var paths = [];
+					paths.push( 'client/components/**/*.scss' );
+					files[ 'client/styles/components/_components.scss' ] = paths;
+					return files;
+				}())
+			},
+			options: { signature: false }
+		},
+		postcss: {
 			options: {
-				textdomain: 'block-preview-host',
+				map: 'build' !== process.argv[2],
+				processors: [
+					require( 'autoprefixer' )( {
+						browsers: [
+							'Android >= 2.1',
+							'Chrome >= 21',
+							'Edge >= 12',
+							'Explorer >= 7',
+							'Firefox >= 17',
+							'Opera >= 12.1',
+							'Safari >= 6.0'
+						],
+						cascade: false
+					} ),
+					require( 'pixrem' ),
+					require('cssnano')({
+						mergeRules: false
+					})
+				]
 			},
-			update_all_domains: {
-				options: {
-					updateDomains: true
-				},
-				src: [ '*.php', '**/*.php', '!\.git/**/*', '!bin/**/*', '!node_modules/**/*', '!tests/**/*' ]
+			dist: {
+				src: 'css/style.css'
 			}
 		},
-
-		wp_readme_to_markdown: {
-			your_target: {
+		eslint: {
+			files: [
+				'client/**/*.js'
+			]
+		},
+		sass: {
+			options: {
+				implementation: require( 'node-sass' ),
+				sourceMap: true,
+				// Don't add source map URL in built version.
+				omitSourceMapUrl: 'build' === process.argv[2],
+				outputStyle: 'expanded'
+			},
+			dist: {
 				files: {
-					'README.md': 'readme.txt'
-				}
-			},
-		},
-
-		makepot: {
-			target: {
-				options: {
-					domainPath: '/languages',
-					exclude: [ '\.git/*', 'bin/*', 'node_modules/*', 'tests/*' ],
-					mainFile: 'block-preview-host.php',
-					potFilename: 'block-preview-host.pot',
-					potHeaders: {
-						poedit: true,
-						'x-poedit-keywordslist': true
-					},
-					type: 'wp-plugin',
-					updateTimestamp: true
+					'css/style.css': 'client/style.scss'
 				}
 			}
 		},
-	} );
+		watch: {
+			css: {
+				files: ['**/*.scss', 'client/components/**/**.scss'],
+				tasks: ['css']
+			},
+			scripts: {
+				files: ['**/*.js'],
+				tasks: ['webpack:build-dev'],
+				options: {
+				  atBegin: true,
+				}
+			},
+			eslint: {
+				files: ['<%= eslint.files %>'],
+				tasks: ['eslint']
+			}
+		}
+	});
 
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
-	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
-	grunt.registerTask( 'default', [ 'i18n','readme' ] );
-	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
-	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-sass-globbing');
+	grunt.loadNpmTasks('grunt-webpack');
+	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-curl');
 
-	grunt.util.linefeed = '\n';
-
+	grunt.registerTask('css', ['sass_globbing', 'sass', 'postcss']);
+	grunt.registerTask('build', ['webpack:build', 'css']);
 };
